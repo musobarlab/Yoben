440561ec3c187b06b649b579d41f33b204e1b1ae
wuriyanto <wuriyanto48@yahoo.co.id>
2019-05-19 14:24:13 +0700

    initial commit

---
+++ src/main/kotlin/com/wuriyanto/yoben/App.kt
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wuriyanto.yoben
+
+/*
+
+  @author wurianto
+  Created on May 19, 2019
+*/
+
+fun main(args : Array<String>) {
+    println("hello world")
+}
---
+++ src/test/kotlin/com/wuriyanto/yoben/AppTest.kt
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wuriyanto.yoben
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+/*
+
+  @author wurianto
+  Created on May 19, 2019
+*/
+
+class AppTest {
+    @Test fun testAssert() : Unit {
+        assertEquals("hello", "hello")
+    }
+}
