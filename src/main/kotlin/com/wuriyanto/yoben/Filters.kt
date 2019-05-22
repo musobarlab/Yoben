f17728581f5da59dd25621f388328a590586ac6b
wuriyanto <wuriyanto48@yahoo.co.id>
2019-05-22 13:48:19 +0700

    add filter functions

--- src/main/kotlin/com/wuriyanto/yoben/Filters.kt
+++ src/main/kotlin/com/wuriyanto/yoben/Filters.kt
@@ -1,2 +1,16 @@
 package com.wuriyanto.yoben
 
+import org.eclipse.jetty.http.HttpStatus
+import spark.Filter
+import spark.Spark
+
+val jwtFilter = Filter{req, res ->
+    val authorization = req.headers("Authorization")
+    if (authorization == null) {
+        Spark.halt(HttpStatus.UNAUTHORIZED_401, "auth not found")
+    }
+
+    println(authorization)
+}
+
+val jsonHeader = Filter{req, res -> res.header("Content-Type", "application/json")}
