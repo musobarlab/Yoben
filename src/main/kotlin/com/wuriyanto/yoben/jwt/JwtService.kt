82d2ac3bcb3367d4c48cd708fe97b710cac90743
wuriyanto <wuriyanto48@yahoo.co.id>
2019-05-23 16:57:53 +0700

    add jwt service and its test

---
+++ src/main/kotlin/com/wuriyanto/yoben/jwt/JwtService.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 wuriyanto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wuriyanto.yoben.jwt
+
+import com.wuriyanto.yoben.utils.Error
+import com.wuriyanto.yoben.utils.ErrorMessage
+import com.wuriyanto.yoben.utils.Ok
+import com.wuriyanto.yoben.utils.Result
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import java.lang.Exception
+import java.security.Key
+
+
+
+interface IJwtService {
+    
+    fun generate(claim: CustomClaim?): Result<String?, ErrorMessage>
+
+    fun validate(accessToken: String?): Result<CustomClaim?, ErrorMessage>
+}
+
+class JwtService(private val privateKey: Key, private val publicKey: Key): IJwtService {
+
+    override fun generate(claim: CustomClaim?): Result<String?, ErrorMessage> {
+        if(claim == null) return Error(ErrorMessage("claim cannot be empty"))
+
+        val jwt: String = Jwts.builder().setSubject(claim?.subject)
+                .setIssuer(claim?.issuer)
+                .setAudience(claim?.audience)
+                .setExpiration(claim?.expiration)
+                .setIssuedAt(claim?.issuedAt)
+                .signWith(SignatureAlgorithm.RS256, privateKey)
+                .compact()
+        return Ok(jwt)
+    }
+
+    override fun validate(accessToken: String?): Result<CustomClaim?, ErrorMessage> {
+        if(accessToken.isNullOrEmpty()) return Error(ErrorMessage("access token cannot be empty"))
+
+        var customClaim: CustomClaim?
+
+        val jwtSplit: List<String> = accessToken?.split(" ")
+
+        if(jwtSplit.size < 2 || jwtSplit.size > 2) return Error(ErrorMessage("access token is not valid"))
+
+        val token = jwtSplit[1]
+
+        try {
+            val c = Jwts.parser().setSigningKey(this.publicKey).parseClaimsJws(token).body
+            customClaim = CustomClaim(c.subject, c.issuer, c.audience, c.expiration)
+            customClaim.issuedAt = c.issuedAt
+            customClaim.notBefore = c.notBefore
+            customClaim.id = customClaim.id
+        } catch (e: Exception) {
+            return Error(ErrorMessage(e.message.toString()))
+        }
+
+        return Ok(customClaim)
+    }
+
+}
--- src/test/kotlin/com/wuriyanto/yoben/jwt/JwtServiceTest.kt
+++ src/test/kotlin/com/wuriyanto/yoben/jwt/JwtServiceTest.kt
@@ -1,4 +1,121 @@
+/*
+ * Copyright 2019 wuriyanto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.wuriyanto.yoben.jwt
 
+import com.wuriyanto.yoben.utils.Error
+import com.wuriyanto.yoben.utils.Ok
+import io.jsonwebtoken.impl.crypto.RsaProvider
+import org.junit.Test
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+
 class JwtServiceTest {
+
+    private val k = RsaProvider.generateKeyPair()
+
+    @Test fun testGenerateAndValidate() {
+        val privateKey = k.private
+        val publicKey = k.public
+
+        val jwtService = JwtService(privateKey, publicKey)
+
+        var calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 17)
+        calendar.set(Calendar.MINUTE, 30)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        calendar.set(Calendar.MONTH, Calendar.AUGUST)
+        calendar.set(Calendar.YEAR, 2019)
+        calendar.set(Calendar.DATE, 13)
+
+        val jwtTokenResult = jwtService.generate(
+                CustomClaim("001", "wuriyanto.com", "my-service", calendar.time)
+        )
+
+        when(jwtTokenResult){
+            is Ok -> {
+                val jwtToken = "Bearer ${jwtTokenResult.value}"
+                println(jwtToken)
+
+                val validateResult = jwtService.validate(jwtToken)
+
+                when(validateResult) {
+                    is Ok -> {
+                        assertNotNull(validateResult.value)
+
+                        val expectedSubject = "001"
+                        val v = validateResult.value
+
+                        assertEquals(expectedSubject, v?.subject)
+
+                    }
+
+                    is Error -> assertNull(validateResult.value)
+                }
+
+            }
+            is Error -> assertNull(jwtTokenResult.value)
+        }
+    }
+
+    @Test fun testGenerateAndValidateErrorEmpty() {
+        val privateKey = k.private
+        val publicKey = k.public
+
+        val jwtService = JwtService(privateKey, publicKey)
+
+        var calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 17)
+        calendar.set(Calendar.MINUTE, 30)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        calendar.set(Calendar.MONTH, Calendar.AUGUST)
+        calendar.set(Calendar.YEAR, 2019)
+        calendar.set(Calendar.DATE, 13)
+
+        val jwtTokenResult = jwtService.generate(null)
+
+        when(jwtTokenResult){
+            is Ok -> {
+                assertNull(jwtTokenResult.value)
+
+                val jwtToken = "Bearer ${jwtTokenResult.value}"
+
+                val validateResult = jwtService.validate(jwtToken)
+
+                when(validateResult) {
+                    is Ok -> {
+                        assertNotNull(validateResult.value)
+
+                        val expectedSubject = "001"
+                        val v = validateResult.value
+
+                        assertEquals(expectedSubject, v?.subject)
+
+                    }
+
+                    is Error -> assertNotNull(validateResult.value)
+                }
+
+            }
+            is Error -> assertNotNull(jwtTokenResult.value)
+        }
+    }
 }
